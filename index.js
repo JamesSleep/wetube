2a31c2cc585d4628f4f55e7fc464f2a198b12914
dongha <jamessleep@donghaui-MacBookPro.local>
2020-08-24 18:54:22 +0900

    #2.4 Handling Routes With Express

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ function handleListening() {
   console.log(`Listening on : http://localhost:${port}`);
 }
 
+function handleHome(req, res) {
+  res.send("home");
+}
+function handleProfile(req, res) {
+  res.send("profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(port, handleListening);
