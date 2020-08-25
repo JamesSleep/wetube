f3a069c9be639bee45609d97f1c1e21a20042199
김동하 <dongha@gimdonghaui-Macmini.local>
2020-08-25 23:46:18 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const port = 4000;
 
-function handleListening() {
-  console.log(`Listening on : http://localhost:${port}`);
-}
+const handleListening = () => console.log(`Listening on : http://localhost:${port}`);
 
-function handleHome(req, res) {
-  res.send("home");
-}
-function handleProfile(req, res) {
-  res.send("profile");
-}
+
+const handleHome = (req, res) => res.send("home");
+
+const handleProfile = (req, res) => res.send("profile");
+
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
