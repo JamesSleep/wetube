592127e2086fd84279a1c67a4f1630c067eaf5c2
김동하 <dongha@gimdonghaui-Macmini.local>
2020-08-31 22:31:57 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const port = 4000;
-
-const handleListening = () => console.log(`Listening on : http://localhost:${port}`);
-
 
 const handleHome = (req, res) => res.send("home");
 
@@ -24,4 +20,6 @@ app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-app.listen(port, handleListening);
+app.use("/user", userRouter)
+
+export default app;
