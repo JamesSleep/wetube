a48a37989dfc7bec044ceb5b6f8bce1524cbb69f
김동하 <dongha@gimdonghaui-Macmini.local>
2020-09-08 22:59:31 +0900

    #2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 32323,
+    title: "Video Cool",
+    description: "iu so pretty",
+    views: 32000,
+    videoFile: "https://youtu.be/K9qlYbwPpvo",
+    creator: {
+      id: 23232,
+      name: "JamesSleep",
+      email: "tyuyiy@naver.com",
+    }
+  },
+  {
+    id: 3222323,
+    title: "red velvet",
+    description: "irin so pretty",
+    views: 14000,
+    videoFile: "https://youtu.be/365hA_iLYZE",
+    creator: {
+      id: 21276,
+      name: "JamesSleep2",
+      email: "tyuyiy@naver.com",
+    }
+  },
+  {
+    id: 123212,
+    title: "Faker",
+    description: "he is legend",
+    views: 3222000,
+    videoFile: "https://www.youtube.com/embed/zge7dU92H_Y",
+    creator: {
+      id: 47621,
+      name: "JamesSleep3",
+      email: "tyuyiy@naver.com",
+    }
+  }
+];
