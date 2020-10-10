c51b049529f05a85d6ebd64ad209536bb2c102b5
dongha <jamessleep@donghaui-MacBookPro.local>
2020-10-11 03:38:34 +0900

    #3.2 Configuring Dot Env

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-mongoose.connect("mongodb://localhost:27017/we-tube", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, 
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 
